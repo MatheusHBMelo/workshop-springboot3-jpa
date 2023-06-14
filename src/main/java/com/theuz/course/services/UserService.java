fa78a5a2961c87df24fbe0299c15afc4f4cd3592
MatheusHBMelo <matheushbmelo@gmail.com>
2023-06-14 09:25:48 -0300

    Service layer, component registration

---
+++ src/main/java/com/theuz/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.theuz.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.theuz.course.repositories.UserRepository;
+
+import com.theuz.course.entities.User;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
