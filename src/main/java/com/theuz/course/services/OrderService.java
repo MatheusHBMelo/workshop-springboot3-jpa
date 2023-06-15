e1a8cebb67fc2b2284cb3178b75e9fb203b9ab0d
MatheusHBMelo <matheushbmelo@gmail.com>
2023-06-15 10:25:17 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/theuz/course/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.theuz.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.theuz.course.repositories.OrderRepository;
+
+import com.theuz.course.entities.Order;
+
+@Service
+public class OrderService {
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
