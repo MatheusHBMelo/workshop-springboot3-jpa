46cc96a8b621ebc6a8a65e2def1dd807ab1b98a1
MatheusHBMelo <matheushbmelo@gmail.com>
2023-06-19 10:39:15 -0300

    Product

---
+++ src/main/java/com/theuz/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.theuz.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.theuz.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
---
+++ src/main/java/com/theuz/course/services/ProductService.java
@@ -0,0 +1,25 @@
+package com.theuz.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.theuz.course.entities.Product;
+import com.theuz.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
